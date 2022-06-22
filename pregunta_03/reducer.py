371e119dfe3ec28acf4968fafc838efef26644fc
Danielmartinezgil <damartinezgi@unal.edu.co>
2022-06-21 19:08:54 -0500

    Ejercicios map_reduce

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+  for line in sys.stdin:
+        numero, llave, valor = line.split(",")
+        valor=int(valor)
+
+        sys.stdout.write(llave + "," + str(valor) + "\n")
